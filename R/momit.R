d61210d07ee57e235bc3789edeab396c1fddaa6b
vbonhomme <bonhomme.vincent@gmail.com>
2018-02-20 10:30:10 +0100

    more appropriate list.files options

--- R/momit.R
+++ R/momit.R
@@ -7,6 +7,10 @@
 #' @param pattern a [`regex`] to feed [list.files] (default to `\\.mom$`)
 #' @param ... more arguments to feed [list.files]
 #'
+#' @note `list.files` is passed with `full.names=TRUE, recursive = TRUE`,
+#' so that full names are returned and all subfolders (if any) will be scrapped.
+#' If you do not want this behaviour, `list.files` manually then continue with one
+#' of `from_*` function.
 #' @return `character` lines that can be momified with [parse_mom]
 #'
 #' @export
@@ -15,7 +19,7 @@ momit <- function(x=getwd(), pattern="mom$", ...){
   # list all paths to patterned files
   if (all(dir.exists(x))){
     x <- x %>%
-      lapply(function(.) list.files(., full.names=TRUE, pattern=pattern, ...)) %>%
+      lapply(function(.) list.files(., full.names=TRUE, recursive = TRUE, pattern=pattern, ...)) %>%
       do.call("c", .) %>%
       gsub("//", "/", .)
   }
