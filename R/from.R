d0bd78813f32de18d5e39c5bfce5fe668ed6bb6a
vbonhomme <bonhomme.vincent@gmail.com>
2018-02-21 13:49:15 +0100

    +from_tps

--- R/from.R
+++ R/from.R
@@ -21,3 +21,27 @@ from_mom <- function(x, ...){
     # for mom, they are just parsed
     parse_mom() %>% momify()
 }
+
+#' @rdname from
+#' @export
+from_tps <- function(x, ...){
+  if (!is.list(x)){
+    x <- harvest(x, pattern="tps$", ...)
+  }
+
+  lapply(x, function(.x) .x %>%
+           # move IMAGE on top and turn it into a collated
+           brush_on_top("LM", "IMAGE") %>%
+           brush_add_tildes("IMAGE=")  %>%
+           # handle covariates (not partitions !)
+           brush_gsub("([[:alnum:]]+)=([[:alnum:]]+)", "\\1 \\2") %>%
+           brush_gsub("(LM).*", "\\1") %>%
+           brush_gsub("(CURVES).*", "\\1") %>%
+           brush_gsub("(POINTS).*", "\\1") %>%
+           # in case we have eg curve \n points \n <some coords> points
+           # remove curve and rename nested partitions
+           brush_remove_empty_partition() %>%
+           brush_rename_partition("~")
+  ) %>%
+    parse_mom() %>% momify()
+}
