10842b09f860fb0372fef48e6eb681c660169c05
vbonhomme <bonhomme.vincent@gmail.com>
2018-02-23 14:00:08 +0100

    fixed write_separately

--- R/write.R
+++ R/write.R
@@ -5,7 +5,8 @@
 #' @param x `character` lines, typically from one of [to]`_*` functions
 #' @param path `character`, where to write the file(s)
 #' @param folder `character`, where to write the file(s) (default ``, ie root of the working directory)
-#' @param extension `character`, the extension to use (default `mom`)
+#' @param extension `character`, the extension to use, with or
+#' without a `.` before extension (default `mom`)
 #'
 #' @name write
 #' @rdname write
@@ -21,6 +22,6 @@ write_single <- function(x, path){
 write_separately <- function(x, folder="", extension=""){
   if (!dir.exists(folder))
     dir.create(folder)
-  paths <- paste0(folder, "/", x$name, extension)
-  silent <- lapply(seq_along(x), function(i) x[i, ] %>% .mom_df_2_mom %>% writeLines(paths[i]))
+  paths <- paste0(folder, "/", names(x), ".", extension) %>% gsub("\\.+", ".", .)
+  silent <- lapply(seq_along(x), function(i) x[[i]] %>% writeLines(paths[i]))
 }
