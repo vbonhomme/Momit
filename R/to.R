f75521bf2a7a83953b0b30856948fa575d475ee2
vincent <bonhomme.vincent@gmail.com>
2018-09-24 15:24:18 +0200

    added to_mom domestics and on Coo

--- R/to.R
+++ R/to.R
@@ -1,6 +1,5 @@
 #' Export morphometric data
 #'
-#' Only Momocs is supported so far.
 #'
 #' It's probably easy enough to build Momocs' objects (and others) from the `mom_df`
 #' to not need many exporters. See vignette.
@@ -11,9 +10,8 @@
 #' @param ... additional parameters to feed Momocs's builders see
 #' [Momocs::Out()], [Momocs::Opn()], [Momocs::Ldk()]
 #'
-#' @aliases to_Momocs
-#' @name to_Coo
-#' @rdname to_Coo
+#' @name to
+#' @rdname to
 #' @export
 to_Out <- function(x, coo="coo", fac, ...){
   if (missing(fac))
@@ -22,7 +20,7 @@ to_Out <- function(x, coo="coo", fac, ...){
               fac=fac, ...)
 }
 
-#' @rdname to_Coo
+#' @rdname to
 #' @export
 to_Opn <- function(x, coo="coo", fac, ...){
   if (missing(fac))
@@ -31,7 +29,7 @@ to_Opn <- function(x, coo="coo", fac, ...){
               fac=fac, ...)
 }
 
-#' @rdname to_Coo
+#' @rdname to
 #' @export
 to_Ldk <- function(x, coo="coo", fac, ...){
   if (missing(fac))
@@ -39,3 +37,89 @@ to_Ldk <- function(x, coo="coo", fac, ...){
   Momocs::Ldk(x[, coo, drop=TRUE],
               fac=fac, ...)
 }
+
+
+# domestic classes --------
+
+#' @rdname to
+#' @export
+to_mom <- function(x, ...){
+  UseMethod("to_mom")
+}
+
+#' @rdname to
+#' @export
+to_mom.matrix <- function(x, ...){
+  # to prevent engineer notation
+  op <- options(scipen=999)
+  on.exit(options(op))
+  # return line by line
+  apply(x, 1, paste0, collapse=" ", ...)
+}
+
+#' @rdname to
+#' @export
+to_mom.array <- function(x, ...){
+  # a2l
+  l <- lapply(1:dim(x)[3], function(i) x[,,i])
+  # if names, use them
+  if (!is.null(names(x)[3]))
+    names(l) <- names(x)[3]
+  # to to_mom.list
+  l %>% to_mom
+}
+
+#' @rdname to
+#' @export
+to_mom.list <- function(x, ...){
+  l <- lapply(x, to_mom, ...)
+  # if no names, dummy names
+  if (is.null(names(l)))
+    n <- paste0("~shp", seq_along(l))
+  # prepend each component with its names
+  l <- mapply(c, n, l, SIMPLIFY = F)
+  # unlist and remove names
+  l %>% unlist %>% `names<-`(NULL)
+}
+
+#' @rdname to
+#' @export
+to_mom.data.frame <- function(x, ...){
+  # if no rownames dummy ones
+  # since (no NULL on data.frame but "1", "2", etc.)
+  if (identical(rownames(x), as.character(1:nrow(x)))){
+    n <- paste0("~shp", 1:nrow(x))
+  } else {
+    # otherwise use them
+    n <- rownames(x)
+  }
+  # each row is momified
+  l <- lapply(1:nrow(x), function(i) .df_2_str(x[i, ]))
+  names(l) <- n
+  # prepend each component with its names
+  l <- mapply(c, n, l, SIMPLIFY = F)
+  # unlist and remove names
+  l %>% unlist %>% `names<-`(NULL)
+}
+
+#' @rdname to
+#' @export
+to_mom.Coo <- function(x, ...){
+  # coo
+  coo_l <- lapply(x$coo, to_mom, ...)
+  # fac
+  fac_l <- lapply(1:nrow(x$fac), function(i) .df_2_str(x$fac[i, ]))
+  # if no names, dummy ones
+  if (is.null(names(x))){
+    n <- paste0("~shp", seq_along(coo_l))
+  } else {
+    n <- paste0("~", names(x))
+  }
+  # concatenate element-wise
+  mapply(c, n, coo_l, fac_l, SIMPLIFY = FALSE) %>%
+  # remove names and unlist and return this beauty
+    unlist %>% `names<-`(NULL)
+}
+
+
+
